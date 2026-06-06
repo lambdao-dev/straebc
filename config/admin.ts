75531e65bccb6aa11a88a020cffe69012943e99d
fah-mili <fanny@lambdao.dev>
2026-06-06 11:39:06 +0200

    [FIX] use env var for admin URL to avoid prod stash workaround

--- config/admin.ts
+++ config/admin.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+  url: env('ADMIN_URL', 'http://localhost:1337'),
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
